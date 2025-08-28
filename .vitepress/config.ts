3917f44c1f4882a78c19fd97ae089415dc8c01ba
Coolfan <coolfan1024@gmail.com>
2025-08-28 22:02:47 +0800

    更新编辑链接，移除不必要的路径部分

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -61,7 +61,7 @@ export default defineConfig({
     },
 
     editLink: {
-      pattern: 'https://github.com/Coooolfan/UniBoard-docs/edit/main/uniboard-docs/:path',
+      pattern: 'https://github.com/Coooolfan/UniBoard-docs/edit/main/:path',
       text: '在 GitHub 上编辑此页'
     },
 
